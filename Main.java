9fdffa62d5987e2ae990f933dfd4bda7e4d2b620
ramcj7742 <ramcj7742@wrdsb.ca>
2020-10-29 15:48:08 +0000

    Finished greeting problem

--- Main.java
+++ Main.java
@@ -5,7 +5,13 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
-  
+  JPanel mainPanel;
+
+  JTextField nameInput;
+
+  JButton greetButton;
+
+  JLabel helloLabel;
 
 
   // Method to assemble our GUI
@@ -18,6 +24,40 @@ public class Main implements Runnable, ActionListener{
     frame.setSize(800,600);
     // shows the window
     frame.setVisible(true);
+
+    //initialize main JPanel
+    mainPanel = new JPanel();
+    //disable layout helpers
+    mainPanel.setLayout(null);
+
+    //initialize input text fields
+    nameInput = new JTextField();
+
+    //set location and size for input text fields
+    nameInput.setBounds(200, 100, 200, 40);
+
+    //initialize greeting button
+    greetButton = new JButton("Say Hello");
+
+    //set location and size for greet button
+    greetButton.setBounds(200, 400, 200, 40);
+
+    //add action listener to button
+    greetButton.addActionListener(this);
+
+    //initialize label to greet user
+    helloLabel = new JLabel();
+
+    //set location and size for label
+    helloLabel.setBounds(200, 250, 300, 40);
+
+    //adds everything to main mainPanel
+    mainPanel.add(nameInput);
+    mainPanel.add(greetButton);
+    mainPanel.add(helloLabel);
+
+    //add panel to window
+    frame.add(mainPanel);
  
     
 
@@ -27,6 +67,12 @@ public class Main implements Runnable, ActionListener{
   public void actionPerformed(ActionEvent e){
     // get the command from the action
     String command = e.getActionCommand();
+
+    //gets user input
+    String userName = nameInput.getText();
+
+    //sets the label as a greeting
+    helloLabel.setText("Hello " + userName);
 
   }
 
